7328ea1f92388078bc1d03ce74585a021eb5b9a0
qimo2001 <qimo1994@gmail.com>
2026-06-14 23:16:49 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'ds-baemin-v1';
+const ASSETS = ['./index.html', './manifest.json',
+  './icons/icon-192.png', './icons/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  // 네트워크 우선, 실패시 캐시
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
